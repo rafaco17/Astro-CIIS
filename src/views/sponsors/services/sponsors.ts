60c53ed52f986b554aa089b126adeb14b87f104b
SebastianRemusgo <remusgosebastian@gmail.com>
2024-09-21 21:55:39 -0500

    add sponsors

---
+++ src/views/sponsors/services/sponsors.ts
@@ -0,0 +1,34 @@
+const sponsors = [
+  {
+    name: "Platzi",
+    description: "Platzi es una plataforma de aprendizaje profesional online",
+    image: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/que-es-un-logo-lays.png",
+  },
+  {
+    name: "Midu",
+    description: "Midu es una plataforma de aprendizaje profesional online",
+    image: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/que-es-un-logo-lays.png",
+  },
+  {
+    name: "Dondominio",
+    description: "Dondominio es una plataforma de aprendizaje profesional online",
+    image: "https://ams3.digitaloceanspaces.com/graffica/2022/12/Three-Bars-adidas-logo-1.jpg-1024x570.webp",
+  },
+  {
+    name: "Lemoncode",
+    description: "Lemoncode es una plataforma de aprendizaje profesional online",
+    image: "https://ams3.digitaloceanspaces.com/graffica/2022/12/Three-Bars-adidas-logo-1.jpg-1024x570.webp",
+  },
+  {
+    name: "Lemoncode",
+    description: "Lemoncode es una plataforma de aprendizaje profesional online",
+    image: "https://ams3.digitaloceanspaces.com/graffica/2022/12/Three-Bars-adidas-logo-1.jpg-1024x570.webp",
+  },
+  {
+    name: "Platzi",
+    description: "Platzi es una plataforma de aprendizaje profesional online",
+    image: "https://ams3.digitaloceanspaces.com/graffica/2022/12/Three-Bars-adidas-logo-1.jpg-1024x570.webp",
+  },
+];
+
+export default sponsors;
