3fa4d71f1c509fc798bbd52fc5c403a17adb43cf
Ardev <john.arias.dev@gmail.com>
2024-10-16 16:49:36 -0500

    Avance de yon

---
+++ src/views/schedules/services/data.ts
@@ -0,0 +1,38 @@
+
+
+const data = [
+  {
+    time: '12:00',  // ADAPTAR
+    name: 'Sebastian dadasdad', // SPEAKER
+    profession: 'Jugador de LOL', // description
+    theme: 'Las ventajas de dejar el LOL', // TOPIC
+    img: '', // avatar
+    day: 'lunes'  //day
+  },
+  {
+    time: '12:00',
+    name: 'Sebastian Remusgo',
+    profession: 'Jugador de LOL',
+    theme: 'Las ventajas de dejar el LOL',
+    img: '',
+    day: 'martes'
+  },
+  {
+    time: '12:00',
+    name: 'Sebastian Remusgo',
+    profession: 'Jugador de LOL',
+    theme: 'Las ventajas de dejar el LOL',
+    img: '',
+    day: 'miercoles'
+  },
+  {
+    time: '12:00',
+    name: 'Sebastian Remusgo',
+    profession: 'Jugador de LOL',
+    theme: 'Las ventajas de dejar el LOL',
+    img: '',
+    day: 'jueves'
+  },
+]
+
+export default data;
