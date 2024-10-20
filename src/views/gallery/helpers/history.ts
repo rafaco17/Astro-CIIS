fd7b34b998da2c43314215e49bfb96fc380e9914
rafaco17 <riechevarriam@unjbg.edu.pe>
2024-10-19 20:34:07 -0500

    Added images in edition module, missing optimiced images

---
+++ src/views/gallery/helpers/history.ts
@@ -0,0 +1,383 @@
+export const imagenesGaleria = [
+  {
+    title: "CIIS XIII",
+    description:
+      "Décimo tercera edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2012,
+    items: [
+      {
+        image: "/ciis-history/ciis-xiii-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+    ],
+  },
+  {
+    title: "CIIS XIV",
+    description:
+      "Décimo cuarta edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2013,
+    items: [
+      {
+        image: "/ciis-history/ciis-xiv-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+      {
+        image: "/ciis-history/ciis-xiv-EventoRelacionado.jpg",
+        name: "Flyer de evento relacionado",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XI",
+    description: "Décimo primero Postmaster del Encuentro de egresados",
+    anio: 2014,
+    items: [
+      {
+        image: "/ciis-history/ciis-xiv-postmasterXI.png",
+        name: "Flyer de postmaster XI",
+      },
+    ],
+  },
+  {
+    title: "CIIS XV",
+    description:
+      "Décimo quinta edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2014,
+    items: [
+      {
+        image: "/ciis-history/ciis-xv-BannerPromocional.png",
+        name: "Banner promocional",
+      },
+      {
+        image: "/ciis-history/ciis-xv-BannerPromocionalLogo.jpg",
+        name: "Banner promocional (logo)",
+      },
+      {
+        image: "/ciis-history/ciis-xv-FotografiaPonencia.jpg",
+        name: "Fotografía de ponencia",
+      },
+    ],
+  },
+  {
+    title: "CIIS XVI",
+    description:
+      "Décimo quinta edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2015,
+    items: [
+      {
+        image: "/ciis-history/ciis-xvi-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+      {
+        image: "/ciis-history/ciis-xvi-FlyerPromocional.png",
+        name: "Flyer promocional",
+      },
+      {
+        image: "/ciis-history/ciis-xvi-FlyerConcursoProgramacion.jpg",
+        name: "Flyer de concurso (programación)",
+      },
+      {
+        image: "/ciis-history/ciis-xvi-auditorio.jpg",
+        name: "Fotografía de auditorío",
+      },
+      {
+        image: "/ciis-history/ciis-xvi-ReconocimientoPonente.jpg",
+        name: "Reconocimiento a ponente",
+      },
+    ],
+  },
+  {
+    title: "CIIS XVII",
+    description:
+      "Décimo séptima edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2016,
+    items: [
+      {
+        image: "/ciis-history/ciis-xvii-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+      {
+        image: "/ciis-history/ciis-xvii-eventoRelacionado.png",
+        name: "Flyer de evento relacionado",
+      },
+      {
+        image: "/ciis-history/ciis-xvii-FlyerConcursoProgramacion.jpg",
+        name: "Flyer de concurso (programación)",
+      },
+      {
+        image: "/ciis-history/ciis-xvii-auditorio.jpg",
+        name: "Fotografía de auditorío",
+      },
+      {
+        image: "/ciis-history/ciis-xvii-FotografiaPonencia.jpg",
+        name: "Fotografía de ponencia",
+      },
+      {
+        image: "/ciis-history/ciis-xvii-organizadores.jpg",
+        name: "Entrega de organizadores",
+      },
+    ],
+  },
+  {
+    title: "CIIS XVIII",
+    description:
+      "Décimo octava edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2017,
+    items: [
+      {
+        image: "/ciis-history/ciis-xviii-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+      {
+        image: "/ciis-history/ciis-xviii-auditorio.jpg",
+        name: "Fotografía de auditorío",
+      },
+      {
+        image: "/ciis-history/ciis-xviii-FotografiaPonencia.jpg",
+        name: "Fotografía de ponencia",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XV",
+    description: "Décimo quinto Postmaster del Encuentro de egresados",
+    anio: 2018,
+    items: [
+      {
+        image: "/ciis-history/ciis-xix-postmasterXV.jpg",
+        name: "Flyer de postmaster XV",
+      },
+      {
+        image: "/ciis-history/4a5143ee-ed48-4bcb-a59a-291aa3c7f329.jpg",
+        name: "Flyer Ponentes",
+      },
+    ],
+  },
+  {
+    title: "CIIS XIX",
+    description:
+      "Décimo novena edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2018,
+    items: [
+      {
+        image: "/ciis-history/ciis-xix-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+      {
+        image: "/ciis-history/ciis-xix-FeriaTecnologica.jpg",
+        name: "Fotografía de feria tecnológica",
+      },
+      {
+        image: "/ciis-history/ciis-xix-auditorio.jpg",
+        name: "Fotografía de auditorío",
+      },
+      {
+        image: "/ciis-history/ciix-xix-ponencia.jpg",
+        name: "Fotografía de ponencia",
+      },
+      {
+        image: "/ciis-history/ciis-xix-entregaCertificado.jpg",
+        name: "Entrega de certificado a ponente",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XVI",
+    description: "Décimo sexto Postmaster del Encuentro de egresados",
+    anio: 2019,
+    items: [
+      {
+        image: "/ciis-history/ciis-xx-postmasterXVI.jpg",
+        name: "Flyer de postmaster XVI",
+      },
+    ],
+  },
+  {
+    title: "CIIS XX",
+    description:
+      "Vigésima edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2019,
+    items: [
+      {
+        image: "/ciis-history/ciis-xx-FlyerDeConcursosCall4Posters.jpg",
+        name: "Flyer de concursos (call for posters)",
+      },
+      {
+        image: "/ciis-history/ciis-xx-feriaTecnologica.jpg",
+        name: "Flyer de feria tecnológica",
+      },
+      {
+        image: "/ciis-history/ciis-xx-inauguracion.jpg",
+        name: "Fotografía de inauguración",
+      },
+      {
+        image: "/ciis-history/ciis-xx-auditorio.jpg",
+        name: "Fotografía de auditorío",
+      },
+      {
+        image: "/ciis-history/ciis-xx-AntesalaEvento.jpg",
+        name: "Fotografía de antesala al evento",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XVII",
+    description: "Décimo séptimo Postmaster del Encuentro de egresados",
+    anio: 2020,
+    items: [
+      {
+        image: "/ciis-history/postmasterxvii.jpg",
+        name: "Flyer de postmaster XVII",
+      },
+    ],
+  },
+  {
+    title: "CIIS XXI",
+    description:
+      "Vigésimo primera edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2020,
+    items: [
+      {
+        image: "/ciis-history/ciis-xxi-flyers-ponentes1.jpg",
+        name: "Flyer de ponentes - parte 1",
+      },
+      {
+        image: "/ciis-history/ciis-xxi-flyers-ponentes2.jpg",
+        name: "Flyer de ponentes - parte 2",
+      },
+      {
+        image: "/ciis-history/ciis-xxi-flyers-ponentes3.jpg",
+        name: "Flyer de ponentes - parte 3",
+      },
+      {
+        image: "/ciis-history/ciis-xxi-discusion.jpg",
+        name: "Flyer de panel de discusión",
+      },
+      {
+        image: "/ciis-history/ciis-xxi-feriaTecnologica.jpg",
+        name: "Flyer de feria tecnológica",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XVIII",
+    description: "Décimo octavo Postmaster del Encuentro de egresados",
+    anio: 2021,
+    items: [
+      {
+        image: "/ciis-history/ciis-xxii-postmater-18.jpg",
+        name: "Flyer de postmaster XVIII",
+      },
+    ],
+  },
+  {
+    title: "CIIS XXII",
+    description:
+      "Vigésimo segunda edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2021,
+    items: [
+      {
+        image: "/ciis-history/ciis-xxii-CronogramaPonencias.jpg",
+        name: "Cronograma de ponencias",
+      },
+      {
+        image: "/ciis-history/ciis-xxii-FlyerPrimerPosterSession.jpg",
+        name: "Flyer de primer poster session",
+      },
+      {
+        image: "/ciis-history/ciis-xxii-call4posters.jpg",
+        name: "Flyer de concursos (call for posters)",
+      },
+      {
+        image: "/ciis-history/ciis-xxii-HACKATHON.jpg",
+        name: "Flyer de concursos (hackathon)",
+      },
+      {
+        image: "/ciis-history/ciis-xxii-organizadores.jpg",
+        name: "Organizadores del evento",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XIX",
+    description: "Décimo noveno Postmaster del Encuentro de egresados",
+    anio: 2022,
+    items: [
+      {
+        image: "/ciis-history/ciis-xxiii-postmasterXIX.png",
+        name: "Flyer de postmaster XIX",
+      },
+    ],
+  },
+  {
+    title: "CIIS XXIII",
+    description:
+      "Vigésimo tercera edición del Congreso Internacional de Informática y Sistemas",
+    anio: 2022,
+    items: [
+      {
+        image: "/ciis-history/ciis-xxiii-FlyerPrincipal.jpg",
+        name: "Flyer principal",
+      },
+      {
+        image: "/ciis-history/ciis-xxiii-CronogramaPonencias.jpg",
+        name: "Cronograma de ponencias",
+      },
+      {
+        image: "/ciis-history/ciis-xxiii-auditorio.jpg",
+        name: "Fotografía de auditorío",
+      },
+      {
+        image: "/ciis-history/ciis-xxiii-talleres.jpg",
+        name: "Fotografía de talleres",
+      },
+      {
+        image: "/ciis-history/ciis-xxiii-Estudiantes.jpg",
+        name: "Fotografía de estudiantes",
+      },
+    ],
+  },
+  {
+    title: "POSTMASTER XX",
+    description: "Vigésimo Postmaster del Encuentro de egresados",
+    anio: 2023,
+    items: [
+      {
+        image: "/ciis-history/dcb6ff12-e544-44df-b392-e268ba054f3a.jpeg",
+        name: "Flyer Principal",
+      },
+      {
+        image: "/ciis-history/1b20d645-ec9c-4e8b-a641-469a077b7f15.jpg",
+        name: "Flyer Ponentes",
+      },
+    ],
+  },
+  {
+    title: "CIIS XXIV",
+    description:
+      "Vigésima cuarta edición del Congreso internacional de informática y sistemas",
+    anio: 2023,
+    items: [
+      {
+        image: "/ciis-history/ciis-xxiv-flyer-main-long.png",
+        name: "Flyer Principal",
+      },
+      {
+        image: "/ciis-history/ciis-xxiv-ponentes.jpg",
+        name: "Fotografía de ponentes",
+      },
+      {
+        image: "/ciis-history/ciis-xxiv-dia-1.jpg",
+        name: "Día 1 de inauguración",
+      },
+      {
+        image: "/ciis-history/ciis-xxiv-delegacion-invitada.jpg",
+        name: "Delegación invitada UNASAM",
+      },
+      {
+        image: "/ciis-history/ciis-xxiv-concurso-robotica.jpg",
+        name: "Fotografía concurso de robótica",
+      },
+    ],
+  },
+].reverse();
