45a0776c60c67f90cb0e8c79386e30364d2585a6
rflimat <raulflimat@gmail.com>
2024-10-20 23:42:45 -0500

    Add alert success

--- src/views/panel/components/account/account.tsx
+++ src/views/panel/components/account/account.tsx
@@ -7,6 +7,7 @@ import * as Yup from "yup";
 import { useDialog } from "../../../../hooks/use-dialog";
 import { URI } from "../../../../helpers/endpoints.ts";
 import { QRCodeCanvas, QRCodeSVG } from "qrcode.react";
+import Dialog from "../../../../components/Dialog.tsx";
 
 const Account = () => {
   const { user, logout, updateUser } = useAuth();
@@ -235,145 +236,166 @@ const Account = () => {
   }
 
   return (
-    <div className="p-2 mx-10 sm:p-4 h-dvh w-[calc(100% - 80px)] bg-slate-950">
-      <div className="font-bold tracking-wider mt-8 text-left select-none">
-        <p className="text-4xl opacity-80">
-          ¡Hola, {user?.name} {user?.lastname}!
-        </p>
-      </div>
+    <>
+      <Dialog
+        style={{ top: 20 }}
+        icon="success"
+        message={messageSuccess}
+        open={successDialog.open}
+        onClose={() => {
+          successDialog.handleClose();
+          setMessageSuccess(null);
+        }}
+      />
+      {messageError && (
+        <Dialog
+          style={{ top: 20 }}
+          icon="warning"
+          message={messageError}
+          open={errorDialog.open}
+          onClose={errorDialog.handleClose}
+        />
+      )}
+      <div className="p-2 mx-10 sm:p-4 h-dvh w-[calc(100% - 80px)] bg-slate-950">
+        <div className="font-bold tracking-wider mt-8 text-left select-none">
+          <p className="text-4xl opacity-80">
+            ¡Hola, {user?.name} {user?.lastname}!
+          </p>
+        </div>
 
-      <div className="mt-6">
-        <h4 className="text-xl font-semibold text-[#7AAEF1]">
-          Datos para la elaboración del Certificado:
-        </h4>
-        <span className="text-sm text-slate-400">
-          Es importante que uses los datos correctos, ya que se usarán para
-          emitir tu certificado.
-        </span>
-        <div className="mt-4 flex justify-center sm:justify-evenly flex-col sm:flex-row">
-          <div className="flex flex-col items-center space-y-4 w-full ">
-            <InputWithButton
-              inputName="document_number"
-              type="text"
-              defaultValue={user?.dni}
-              label="Documento de identidad"
-              placeholder="Ingrese su N° documento identidad"
-              disabled={true}
-            />
-            <InputWithButton
-              inputName="name"
-              type="text"
-              label="Nombres"
-              placeholder="Ingrese sus nombres"
-              inputProps={formikName.getFieldProps("name")}
-              touched={formikName.touched.name}
-              error={formikName.errors.name}
-              onSave={formikName.handleSubmit}
-            />
-            <InputWithButton
-              inputName="lastname"
-              type="text"
-              label="Apellidos"
-              placeholder="Ingrese sus apellidos"
-              inputProps={formikLastname.getFieldProps("lastname")}
-              touched={formikLastname.touched.lastname}
-              error={formikLastname.errors.lastname}
-              onSave={formikLastname.handleSubmit}
-            />
-            <InputWithButton
-              inputName="contact_number"
-              type="tel"
-              label="Número de contacto"
-              placeholder="Ingrese su número de contacto"
-              inputProps={formikPhone.getFieldProps("phone")}
-              touched={formikPhone.touched.phone}
-              error={formikPhone.errors.phone}
-              onSave={formikPhone.handleSubmit}
-            />
-          </div>
-          <div className="w-full mt-8 sm:mt-0 flex items-center justify-center ">
-            <div className="space-y-4 flex flex-col items-center">
-              <div className="w-[200px] h-[200px] bg-white rounded-sm">
-                <QRCodeCanvas
-                  ref={qrRef}
-                  size={200}
-                  id="QRcredencial"
-                  value={JSON.stringify(user, [
-                    "id",
-                    "dni",
-                    "role",
-                    "name",
-                    "lastname",
-                  ])}
-                  marginSize={2}
-                />
-              </div>
-              <div className="space-y-1">
-                <span className="text-center block text-xl text-green-400 font-bold">
-                  Credencial QR para la asistencia
-                </span>
-                <button className="w-full px-6 py-2 border border-stone-500 rounded-full hover:bg-slate-600 transition-colors" onClick={handleDownload}>
-                  Descargar Credencial
-                </button>
+        <div className="mt-6">
+          <h4 className="text-xl font-semibold text-[#7AAEF1]">
+            Datos para la elaboración del Certificado:
+          </h4>
+          <span className="text-sm text-slate-400">
+            Es importante que uses los datos correctos, ya que se usarán para
+            emitir tu certificado.
+          </span>
+          <div className="mt-4 flex justify-center sm:justify-evenly flex-col sm:flex-row">
+            <div className="flex flex-col items-center space-y-4 w-full ">
+              <InputWithButton
+                inputName="document_number"
+                type="text"
+                defaultValue={user?.dni}
+                label="Documento de identidad"
+                placeholder="Ingrese su N° documento identidad"
+                disabled={true}
+              />
+              <InputWithButton
+                inputName="name"
+                type="text"
+                label="Nombres"
+                placeholder="Ingrese sus nombres"
+                inputProps={formikName.getFieldProps("name")}
+                touched={formikName.touched.name}
+                error={formikName.errors.name}
+                onSave={formikName.handleSubmit}
+              />
+              <InputWithButton
+                inputName="lastname"
+                type="text"
+                label="Apellidos"
+                placeholder="Ingrese sus apellidos"
+                inputProps={formikLastname.getFieldProps("lastname")}
+                touched={formikLastname.touched.lastname}
+                error={formikLastname.errors.lastname}
+                onSave={formikLastname.handleSubmit}
+              />
+              <InputWithButton
+                inputName="contact_number"
+                type="tel"
+                label="Número de contacto"
+                placeholder="Ingrese su número de contacto"
+                inputProps={formikPhone.getFieldProps("phone")}
+                touched={formikPhone.touched.phone}
+                error={formikPhone.errors.phone}
+                onSave={formikPhone.handleSubmit}
+              />
+            </div>
+            <div className="w-full mt-8 sm:mt-0 flex items-center justify-center ">
+              <div className="space-y-4 flex flex-col items-center">
+                <div className="w-[200px] h-[200px] bg-white rounded-sm">
+                  <QRCodeCanvas
+                    ref={qrRef}
+                    size={200}
+                    id="QRcredencial"
+                    value={JSON.stringify(user, [
+                      "id",
+                      "dni",
+                      "role",
+                      "name",
+                      "lastname",
+                    ])}
+                    marginSize={2}
+                  />
+                </div>
+                <div className="space-y-1">
+                  <span className="text-center block text-xl text-green-400 font-bold">
+                    Credencial QR para la asistencia
+                  </span>
+                  <button className="w-full px-6 py-2 border border-stone-500 rounded-full hover:bg-slate-600 transition-colors" onClick={handleDownload}>
+                    Descargar Credencial
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-4 flex justify-evenly items-start flex-col sm:flex-row gap-x-4 gap-y-4 sm:gap-y-0">
-        <InputWithButton
-          inputName="study_center"
-          type="text"
-          label="Centro de estudios"
-          placeholder="Ingrese su centro de estudios"
-          inputProps={formikstudycenter.getFieldProps("studycenter")}
-          touched={formikstudycenter.touched.studycenter}
-          error={formikstudycenter.errors.studycenter}
-          onSave={formikstudycenter.handleSubmit}
-        />
-        <InputWithButton
-          inputName="career"
-          type="tel"
-          label="Carrera profesional"
-          placeholder="Ingrese su carrera profesional"
-          inputProps={formikCareer.getFieldProps("career")}
-          touched={formikCareer.touched.career}
-          error={formikCareer.errors.career}
-          onSave={formikCareer.handleSubmit}
-        />
-      </div>
-
-      <div className="mt-8 pb-8">
-        <h4 className="text-xl font-semibold text-[#7AAEF1]">
-          Datos para el acceso de la Cuenta:
-        </h4>
-        <span className="text-sm text-slate-400">
-          Asegúrate de ingresar un correo válido, ya que tu certificado se
-          enviará a esa dirección.
-        </span>
         <div className="mt-4 flex justify-evenly items-start flex-col sm:flex-row gap-x-4 gap-y-4 sm:gap-y-0">
           <InputWithButton
-            inputName="email"
-            type="email"
-            defaultValue={user?.email}
-            label="Correo electrónico"
-            placeholder="Ingrese su email"
-            disabled={true}
+            inputName="study_center"
+            type="text"
+            label="Centro de estudios"
+            placeholder="Ingrese su centro de estudios"
+            inputProps={formikstudycenter.getFieldProps("studycenter")}
+            touched={formikstudycenter.touched.studycenter}
+            error={formikstudycenter.errors.studycenter}
+            onSave={formikstudycenter.handleSubmit}
           />
           <InputWithButton
-            type="password"
-            inputName="password"
-            label="Cambiar contraseña"
-            placeholder="Ingrese su contraseña"
-            inputProps={formikPass.getFieldProps("password")}
-            touched={formikPass.touched.password}
-            error={formikPass.errors.password}
-            onSave={formikPass.handleSubmit}
+            inputName="career"
+            type="tel"
+            label="Carrera profesional"
+            placeholder="Ingrese su carrera profesional"
+            inputProps={formikCareer.getFieldProps("career")}
+            touched={formikCareer.touched.career}
+            error={formikCareer.errors.career}
+            onSave={formikCareer.handleSubmit}
           />
         </div>
+
+        <div className="mt-8 pb-8">
+          <h4 className="text-xl font-semibold text-[#7AAEF1]">
+            Datos para el acceso de la Cuenta:
+          </h4>
+          <span className="text-sm text-slate-400">
+            Asegúrate de ingresar un correo válido, ya que tu certificado se
+            enviará a esa dirección.
+          </span>
+          <div className="mt-4 flex justify-evenly items-start flex-col sm:flex-row gap-x-4 gap-y-4 sm:gap-y-0">
+            <InputWithButton
+              inputName="email"
+              type="email"
+              defaultValue={user?.email}
+              label="Correo electrónico"
+              placeholder="Ingrese su email"
+              disabled={true}
+            />
+            <InputWithButton
+              type="password"
+              inputName="password"
+              label="Cambiar contraseña"
+              placeholder="Ingrese su contraseña"
+              inputProps={formikPass.getFieldProps("password")}
+              touched={formikPass.touched.password}
+              error={formikPass.errors.password}
+              onSave={formikPass.handleSubmit}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
